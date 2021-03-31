bfc9518e360d3b19f054609d6f104adb55b1036e
DevOps(The Artisans) <fwilingafum@vodacom.co.tz>
2021-03-31 11:38:26 +0300

    added new files

--- app/public/js/de-register.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	document.querySelector("#deRegBtn1").addEventListener("click", function() {
-	    $("#fingerData").val(CapturedData);
-	    $("#fingerCode").val(CapturedFinger);
-	});
-});
--- app/resources/views/includes/auth/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="col-12 mt-3" align="center">
-    <span>&COPY;<?php echo date("Y"); ?></span>
-    <span>VTL BioKYC Portal</span>
-</div>
